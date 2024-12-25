580c645d3a2a59d2b7b88bde2ba16868ab886869
Uju447 <ujunmanwagboso@gmail.com>
2024-12-25 13:46:14 +0100

    Updated team members section

--- src/components/teamMembers.tsx
+++ src/components/teamMembers.tsx
@@ -1,5 +1,197 @@
 export const TeamMembers = () => {
     return(
-        <div>Team members</div>
-    )
-}
+            <div style="backgroundColor: #0a2e47; padding: 40px; color:rgb(255, 255, 255); textAlign: center; ">
+                <h1>Who are there to support you?</h1>
+                <h2>Team Members</h2>
+                <p>We are the people who make up Nile Startup Campus</p>
+
+                <div class="row container">
+
+                    <div class="col l4 m4 s12" style=" gap: 20px; marginTop: 40px; backgroundColor: #f1f5f9; borderRadius: 10px; textAlign: center; padding: 10px;">
+                            <div><Image
+                                src="/teamMembers-img/Placeholder image.png"
+                                style="
+                                height=150px;
+                                borderRadius: 8px;
+                                backgroundSize: cover;
+                                backgroundPosition: center;"
+                            /></div>
+                            <h3 style=" margin: 10px 0 5px; color:rgb(0, 0, 0)">Idris Shaaba</h3>
+                            <p style="margin: 0 5px; color:rgb(71, 71, 71)">CEO</p>
+                            <p style="margin: 0 5px; color:rgb(71, 71, 71)">Chief Executive Officer (CEO)</p>
+                    </div>
+
+                    <div class="col l4 m4 s12" style=" gap: 20px; marginTop: 40px; backgroundColor: #f1f5f9; borderRadius: 10px; textAlign: center; padding: 10px;">
+                            <div><Image
+                                src="/teamMembers-img/shafatu.png"
+                                style="
+                                height=150px;
+                                borderRadius: 8px;
+                                backgroundSize: cover;
+                                backgroundPosition: center;"
+                            /></div>
+                            <h3 style=" margin: 10px 0 5px;  color:rgb(0, 0, 0)">Shafaatu Aminu Danmusa</h3>
+                            <p style="margin: 0 5px; color:rgb(71, 71, 71);">COO</p>
+                            <p style="margin: 0 5px; color:rgb(71, 71, 71);">Chief Operations Officer</p>
+                    </div>
+
+                    <div class="col l4 m4 s12" style=" gap: 20px; marginTop: 40px; backgroundColor: #f1f5f9; borderRadius: 10px; textAlign: center; padding: 10px;">
+                            <div><Image
+                                src="/teamMembers-img/zeenat.png"
+                                style="
+                                height=150px;
+                                borderRadius: 8px;
+                                backgroundSize: cover;
+                                backgroundPosition: center;"
+                            /></div>
+                            <h3 style=" margin: 10px 0 5px;  color:rgb(0, 0, 0)">Zeenat Harith Sulayman</h3>
+                            <p style="margin: 0 5px; color:rgb(71, 71, 71)">Exec. Sec.</p>
+                            <p style="margin: 0 5px; color:rgb(71, 71, 71)">Executive Secretary</p>
+                    </div>
+
+                </div>
+
+
+                <div class="row container">
+
+                    <div class="col l4 m4 s12" style=" gap: 20px; marginTop: 40px; backgroundColor: #f1f5f9; borderRadius: 10px; textAlign: center; padding: 10px;">
+                            <div><Image
+                                src="/teamMembers-img/raji.png"
+                                style="
+                                height=150px;
+                                borderRadius: 8px;
+                                backgroundSize: cover;
+                                backgroundPosition: center;"
+                            /></div>
+                            <h3 style=" margin: 10px 0 5px; color:rgb(0, 0, 0)">Muzzamil Raji</h3>
+                            <p style="margin: 0 5px; color:rgb(71, 71, 71)">CTO</p>
+                            <p style="margin: 0 5px; color:rgb(71, 71, 71)">Chief Technical Officer</p>
+                    </div>
+
+                    <div class="col l4 m4 s12" style=" gap: 20px; marginTop: 40px; backgroundColor: #f1f5f9; borderRadius: 10px; textAlign: center; padding: 10px;">
+                            <div><Image
+                                src="/teamMembers-img/zainab.png"
+                                style="
+                                height=150px;
+                                borderRadius: 8px;
+                                backgroundSize: cover;
+                                backgroundPosition: center;"
+                            /></div>
+                            <h3 style=" margin: 10px 0 5px;  color:rgb(0, 0, 0)">Zainab Sadeeq Sule-Iko</h3>
+                            <p style="margin: 0 5px; color:rgb(71, 71, 71);">CMO</p>
+                            <p style="margin: 0 5px; color:rgb(71, 71, 71);">Chief Marketing Officer </p>
+                    </div>
+
+                    <div class="col l4 m4 s12" style=" gap: 20px; marginTop: 40px; backgroundColor: #f1f5f9; borderRadius: 10px; textAlign: center; padding: 10px;">
+                            <div><Image
+                                src="/teamMembers-img/kelechi.png"
+                                style="
+                                height=150px;
+                                borderRadius: 8px;
+                                backgroundSize: cover;
+                                backgroundPosition: center;"
+                            /></div>
+                            <h3 style=" margin: 10px 0 5px;  color:rgb(0, 0, 0)">Kelechi Theresa Chukwu </h3>
+                            <p style="margin: 0 5px; color:rgb(71, 71, 71)">CFO</p>
+                            <p style="margin: 0 5px; color:rgb(71, 71, 71)">Chief Financial Officer</p>
+                    </div>
+
+                </div>
+
+
+                <div class="row container">
+
+                    <div class="col l4 m4 s12" style=" gap: 20px; marginTop: 40px; backgroundColor: #f1f5f9; borderRadius: 10px; textAlign: center; padding: 10px;">
+                            <div><Image
+                                src="/teamMembers-img/nabilah.png"
+                                style="
+                                height=150px;
+                                borderRadius: 8px;
+                                backgroundSize: cover;
+                                backgroundPosition: center;"
+                            /></div>
+                            <h3 style=" margin: 10px 0 5px; color:rgb(0, 0, 0)">Nabilah Abdulrahim</h3>
+                            <p style="margin: 0 5px; color:rgb(71, 71, 71)">PM</p>
+                            <p style="margin: 0 5px; color:rgb(71, 71, 71)">Program Manager</p>
+                    </div>
+
+                    <div class="col l4 m4 s12" style=" gap: 20px; marginTop: 40px; backgroundColor: #f1f5f9; borderRadius: 10px; textAlign: center; padding: 10px;">
+                            <div><Image
+                                src="/teamMembers-img/ransom.png"
+                                style="
+                                height=150px;
+                                borderRadius: 8px;
+                                backgroundSize: cover;
+                                backgroundPosition: center;"
+                            /></div>
+                            <h3 style=" margin: 10px 0 5px;  color:rgb(0, 0, 0)">Ransom Philemon</h3>
+                            <p style="margin: 0 5px; color:rgb(71, 71, 71);">Asst. PM</p>
+                            <p style="margin: 0 5px; color:rgb(71, 71, 71);">Assistant Program Manager</p>
+                    </div>
+
+                    <div class="col l4 m4 s12" style=" gap: 20px; marginTop: 40px; backgroundColor: #f1f5f9; borderRadius: 10px; textAlign: center; padding: 10px;">
+                            <div><Image
+                                src="/teamMembers-img/khadijah.png"
+                                style="
+                                height=150px;
+                                borderRadius: 8px;
+                                backgroundSize: cover;
+                                backgroundPosition: center;"
+                            /></div>
+                            <h3 style=" margin: 10px 0 5px;  color:rgb(0, 0, 0)">Khadija Sani Musa</h3>
+                            <p style="margin: 0 5px; color:rgb(71, 71, 71)"></p>
+                            <p style="margin: 0 5px; color:rgb(71, 71, 71)">Content Creator</p>
+                    </div>
+
+                </div>  
+
+
+                <div class="row container">
+
+                    <div class="col l4 m4 s12" style=" gap: 20px; marginTop: 40px; backgroundColor: #f1f5f9; borderRadius: 10px; textAlign: center; padding: 10px;">
+                            <div><Image
+                                src="/teamMembers-img/Placeholder image.png"
+                                style="
+                                height=150px;
+                                borderRadius: 8px;
+                                backgroundSize: cover;
+                                backgroundPosition: center;"
+                            /></div>
+                            <h3 style=" margin: 10px 0 5px; color:rgb(0, 0, 0)">Claire Erhuanga</h3>
+                            <p style="margin: 0 5px; color:rgb(71, 71, 71)">PM</p>
+                            <p style="margin: 0 5px; color:rgb(71, 71, 71)">Project Manager</p>
+                    </div>
+
+                    <div class="col l4 m4 s12" style=" gap: 20px; marginTop: 40px; backgroundColor: #f1f5f9; borderRadius: 10px; textAlign: center; padding: 10px;">
+                            <div><Image
+                                src="/teamMembers-img/farida.png"
+                                style="
+                                height=150px;
+                                borderRadius: 8px;
+                                backgroundSize: cover;
+                                backgroundPosition: center;"
+                            /></div>
+                            <h3 style=" margin: 10px 0 5px;  color:rgb(0, 0, 0)">Faridah Okeniyi</h3>
+                            <p style="margin: 0 5px; color:rgb(71, 71, 71);"></p>
+                            <p style="margin: 0 5px; color:rgb(71, 71, 71);">Content Creator</p>
+                    </div>
+
+                    <div class="col l4 m4 s12" style=" gap: 20px; marginTop: 40px; backgroundColor: #f1f5f9; borderRadius: 10px; textAlign: center; padding: 10px;">
+                            <div><Image
+                                src="/teamMembers-img/drumar.png"
+                                style="
+                                height=150px;
+                                borderRadius: 8px;
+                                backgroundSize: cover;
+                                backgroundPosition: center;"
+                            /></div>
+                            <h3 style=" margin: 10px 0 5px;  color:rgb(0, 0, 0)">Dr. Umar Adam I. PhD.</h3>
+                            <p style="margin: 0 5px; color:rgb(71, 71, 71)"></p>
+                            <p style="margin: 0 5px; color:rgb(71, 71, 71)">Lecturer / Club Advisor</p>
+                    </div>
+
+                </div>
+
+
+            </div>
+)}
