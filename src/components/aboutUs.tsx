137297cb6b148cdd597e18d5d58b88ecf52150a7
Niabari-NI-2 <niabari.ni@gmail.com>
2025-01-04 01:00:08 +0100

    Other part that didn't commit

--- src/components/aboutUs.tsx
+++ src/components/aboutUs.tsx
@@ -3,31 +3,34 @@ import Image from "next/image";
 export const AboutUs = () => {
     return(
         
-        <div className="">
-            <div>Who are we?</div>
-            <div >About Us</div>
+        <div className="sm:container sm:mx-auto rounded-lg shadow-lg text-[#333333]">
+            <div className="text-center text-sm">Who are we?</div>
+            <div className="text-center text-2xl font-bold">About Us</div>
             
-            <div className="grid grid-cols-2 gap-8">
-                <div>
-                    <p><span>Nile Startup Campus</span> equips students with the necessary skills to innovate and enables them to realize their ideas.</p>
+            <div className="grid md:grid-cols-2">
+                <div className="m-32 mt-10 h-331 w-464">
+                    <div className="text-md font-light text-justify tracking-tighter">
+                        <p><span className="text-lg font-normal">Nile Startup Campus</span> equips students with the necessary skills to innovate and enables them to realize their ideas.</p>
+                        <br></br>
+                        <p>It's a community where aspiring entrepreneurs, innovators, and early-stage businesses can access resources, mentorship, networking opportunities, and education to help them grow and succeed.</p>
+                    </div>
                     <br></br>
-                    <p>It's a community where aspiring entrepreneurs, innovators, and early-stage businesses can access resources, mentorship, networking opportunities, and education to help them grow and succeed.</p>
+                    <button className="bg-[#113045] text-[#FFFFFF] rounded-full p-2 pr-4 pl-4">See more</button>
                 </div>
                 
-                <div>
-                    <Image
-                            className="rounded-full"
-                            src="/About-img/AboutPic.svg"
-                            alt="2023 Orientation Info Session"
-                            width={538}
-                            height={361}
-                    />
-                </div>
-            </div>
-            
-            <button className="bg-[#113045] text-[#FFFFFF] rounded-full md:py-[12px] py-8 md:px-[20px] px-6">See more</button>
 
-            <div>2023 Orientation Info Session</div>
+                <div className="relative">
+                    <div className="container relative mr-3 mt-10 ml-9 mb-16 w-500 h-300"> 
+                        <img src="/About-img/AboutPic.svg" alt="2023 Orientation Info Session" className="rounded-lg"/>
+                        <div className="absolute -bottom-2 sm:right-24">
+                            <div className="bg-white rounded-lg p-5">
+                                <p className="text-black text-md font-normal text-center">2023<br></br>Orientation<br></br>Info Session</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
 
     )
