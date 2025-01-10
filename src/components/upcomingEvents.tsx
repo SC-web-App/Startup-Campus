20edf997fc999ca217f01bea67e178e6a074ef5c
aiieesha01 <aishaawaisu01@gmail.com>
2025-01-10 01:01:25 +0100

    Initial commit

--- src/components/upcomingEvents.tsx
+++ src/components/upcomingEvents.tsx
@@ -1,5 +1,58 @@
 export const UpcomingEvents = () => {
     return(
-        <div>Upcoming Events</div>
+        <div className="sm:container sm:mx-auto rounded-lg shadow-lg text-[#333333]">
+        
+        <div className="text-center text-sm mt-12"> What do we have in store? 
+        <div className="text-center text-2xl font-bold ">Upcoming Events</div>
+        <div className="text-center text-l ">Take a glance at the events and workshops we have planned</div>
+        
+
+        <div className="grid grid-cols-3 items-center gap-10">
+        <div className="relative">
+                    <div className="container relative mr-3 mt-10 ml-9 mb-16 w-500 h-300"> 
+                        
+                        <img src="/About-img/AboutPic.svg" alt="2023 Orientation Info Session" className="rounded-lg"/>
+                        <div className="absolute -bottom-11 sm:left-">
+                            <div className="bg-white rounded-lg p-5">
+                                <p className="text-black text-md font-normal text-center">Startup 4.0 Competition<br></br>Collaborate to develop sustainable solutions  addressing the UN's SDG<br></br></p>
+                                <button className="bg-blue-900 text-white px-4 py-2 rounded hover:bg-blue-700 block mt-2">Register</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+  
+
+                <div className="relative">
+                    <div className="container relative mr-3 mt-10 ml-9 mb-16 w-500 h-300"> 
+                        <img src="/About-img/AboutPic.svg" alt="2023 Orientation Info Session" className="rounded-lg"/>
+                        <div className="absolute -bottom-11 sm:left-">
+                            <div className="bg-white rounded-lg p-5">
+                                <p className="text-black text-md font-normal text-center">Startup 4.0 Competition<br></br>Collaborate to develop sustainable solutions addressing the UN's SDG<br></br></p>
+                                <button className="bg-blue-900 text-white px-4 py-2 rounded hover:bg-blue-700 block mt-2">Register</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="relative">
+                    <div className="container relative mr-3 mt-10 ml-9 mb-16 w-500 h-300"> 
+                        <img src="/About-img/AboutPic.svg" alt="2023 Orientation Info Session" className="rounded-lg"/>
+                        <div className="absolute -bottom-11 sm:left-">
+                            <div className="bg-white rounded-lg p-5">
+                                <p className="text-black text-md font-normal text-center">Startup 4.0 Competition<br></br>Collaborate to develop sustainable solutionsh addressing the UN's SDG<br></br></p>
+                                <button className="bg-blue-900 text-white px-4 py-2 rounded hover:bg-blue-700 block mt-2">Register</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+</div>
+
+
+    
+
+        </div>
+        </div>
+        
+      
     )
 }
